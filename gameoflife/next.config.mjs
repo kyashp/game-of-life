af2d88df23d54096b7736436fe01bca5549b7c93
Wei Yong <weiyongt203@gmail.com>
2025-10-05 15:47:01 +0800

    Added default redirect to Landing_Page

--- gameoflife/next.config.mjs
+++ gameoflife/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects(){
+        return [
+            {
+                source: '/',
+                destination: '/Landing_Page',
+                permanent: true, // This is a permanent redirect
+            }
+        ]
+    }
+};
 
 export default nextConfig;
