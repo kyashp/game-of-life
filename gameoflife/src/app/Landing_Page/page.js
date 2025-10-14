94031c018b5a2d2e3dfd6369925e6f3b2503179a
Abhinav-2223 <narneabhinav123@gmail.com>
2025-10-14 21:21:32 +0800

    added some checklists

--- gameoflife/src/app/Landing_Page/page.js
+++ gameoflife/src/app/Landing_Page/page.js
@@ -10,7 +10,7 @@ export default function Landing() {
       
       {/* Left Section */}
       <div className="flex-1 flex flex-col items-center justify-center p-8">
-        <h1 className="text-4xl font-bold mb-4">Strategise your parenthood finances with Game of Life</h1>
+        <h1 className="text-5xl font-bold mb-4">Strategise your parenthood finances with Game of Life</h1>
         <p className="text-lg text-gray-600 text-center">
           Simulate your child’s future in Singapore, make strategic decisions, and secure your finances with our platform. 
           Sign up for an account to save progress or play as guest.
