f6cb35ad17dc57663d7e34ad882625fb68d7b2c1
Yash <154773174+kyashp@users.noreply.github.com>
2025-10-15 12:44:33 +0800

    fixed errors on landing page

--- gameoflife/src/app/Landing_Page/page.js
+++ gameoflife/src/app/Landing_Page/page.js
@@ -9,18 +9,15 @@ export default function Landing() {
     <div className="min-h-screen flex bg-[#fefcf3]">
       
       {/* Left Section */}
-      <div className="flex-1 flex flex-col items-center justify-center p-8">
-        <h1 className="text-5xl font-bold mb-4">Strategise your parenthood finances with Game of Life</h1>
-        <p className="text-lg text-gray-600 text-center">
-          Simulate your child&apos;s future in Singapore, make strategic decisions, and secure your finances with our platform. 
-      <div className="flex-1 flex flex-col items-center justify-center p-8 "
-      style={{
-        backgroundImage: `
-          linear-gradient(to right, #e5e7eb 1px, transparent 1px),
-          linear-gradient(to bottom, #e5e7eb 1px, transparent 1px)
-        `,
-        backgroundSize: '40px 40px'
-      }}
+      <div 
+        className="flex-1 flex flex-col items-center justify-center p-8"
+        style={{
+          backgroundImage: `
+            linear-gradient(to right, #e5e7eb 1px, transparent 1px),
+            linear-gradient(to bottom, #e5e7eb 1px, transparent 1px)
+          `,
+          backgroundSize: '40px 40px'
+        }}
       >
         <h1 className="text-5xl font-bold mb-4">Strategise your parenthood <a className='text-[#f47068]'>finances</a> with Game of Life</h1>
         <p className="text-2xl text-gray-600 text-center">
@@ -107,10 +104,6 @@ export default function Landing() {
                     Play as Guest
                   </button>
                 </div>
-
-
-
-              
             </>
           ) : (
             /* Sign Up Form */
@@ -162,13 +155,3 @@ export default function Landing() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
